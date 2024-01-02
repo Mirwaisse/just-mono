cc18533755c692d36ff827cbd9cb62565755d37e
Mirwaisse <compsi75@gmail.com>
2024-01-02 23:28:21 +0100

    Just Mono v2

--- README.md
+++ README.md
@@ -1,15 +1,18 @@
-# Just Mono
+# Just Mono v2
 A minimalistic Anki addon for making text monospace.
 
 They have superscript and subscript, but not monospace? This addon fixes that.
 
-## Usage
-1. Select the text you want to mono-ify.
-2. Click the black square button or press `Alt+c`.
+Works with Anki 23.10+.
 
-## Customising
-- All monospace decorations (i.e. `<code>` tags) from this addon has a `just-mono` class. Do whatever you want with it.
-- You can change the hotkey in the config.
+## Usage
+- Select the text you want to mono-ify.
+- 2 options:
+  - Click the button "Add code" or press `Alt+X` to wrap the selection with `<code class="just-code">...</code>`.
+  - Click the button "Add pre+code" or press `Alt+C` to wrap the selection with `<pre><code class="just-pre-code">...</code></pre>`.
+
+## Customizing
+- You can change the hotkey in the `config.json` file in your add-on folder.
 
 ## Credits
 This project is a stripped-down version of [Syntax Highlighting FORK](https://github.com/ijgnd/anki__syntax-highlighting)
--- __init__.py
+++ __init__.py
@@ -4,27 +4,32 @@ from aqt import mw
 from aqt.qt import *
 from anki.hooks import addHook
 
-def get_key():
+def get_key(command):
     conf = mw.addonManager.getConfig(__name__)
-    return conf.get("hotkey", "") if conf else ""
+    return conf.get(command, "") if conf else ""
 
 def format_key(k):
-    return QKeySequence(k).toString(QKeySequence.NativeText)
+    return QKeySequence(k).toString(QKeySequence.SequenceFormat.NativeText)
 
-def wrap_mono(editor):
-    class_string = 'class="just-mono"'
+def wrap_code(editor):
+    class_string = 'class="just-code"'
     editor.web.eval(f"wrap('<code {class_string}>', '</code>')")
 
+def wrap_pre_code(editor):
+    class_string = 'class="just-pre-code"'
+    editor.web.eval(f"wrap('<pre><code {class_string}>', '</code></pre>')")
+
 def setupEditorButtonsFilter(buttons, editor):
-    key = get_key()
-    b = editor.addButton(
-            os.path.join(os.path.dirname(__file__), "justmono.png"),
-            "justmono_button",
-            wrap_mono,
-            tip=f"Just Mono ({format_key(key)})",
-            keys=key
-        )
-    buttons.append(b)
+    for command, function in [["code", wrap_code], ["pre+code", wrap_pre_code]]:
+        key = get_key(command)
+        b = editor.addButton(
+                os.path.join(os.path.dirname(__file__), "justmono.png"),
+                f"justmono_{command}",
+                function,
+                tip=f"Add {command} ({format_key(key)})",
+                keys=key
+            )
+        buttons.append(b)
     return buttons
 
 addHook("setupEditorButtons", setupEditorButtonsFilter)
